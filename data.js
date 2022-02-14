e2a40b652d41f70fdef6c4a6b1085173ae325c88
selatran <80890760+selatran@users.noreply.github.com>
2022-02-13 18:40:00 -0800

    new files

---
+++ data.js
@@ -0,0 +1,101 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-51877617988_5c18b6a5f4_o",
+      "name": "51877617988_5c18b6a5f4_o",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.6738469458736045,
+        "pitch": 0.08880283037888503,
+        "fov": 1.4347569566174885
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04624289677540716,
+          "pitch": 0.0027816244909644183,
+          "rotation": 0,
+          "target": "1-51876525177_2b6501afc4_o"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.560716412430704,
+          "pitch": -0.2788068950409279,
+          "title": "Route 1",
+          "text": "enter here to go somewhere else."
+        }
+      ]
+    },
+    {
+      "id": "1-51876525177_2b6501afc4_o",
+      "name": "51876525177_2b6501afc4_o",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1728,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.412556770467143,
+          "pitch": -0.04248755599808263,
+          "rotation": 0,
+          "target": "0-51877617988_5c18b6a5f4_o"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.5951934023604082,
+          "pitch": 0.08439746803632353,
+          "title": "go back to the bridge",
+          "text": "dfasjndfawlmfksdflksa gk sdkga slkdgm"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
